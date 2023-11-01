ba38161150ee2c939b80c0afabdedf83c68c7943
PainIsWhenSomeBodyDropT3 <thats_change_everything@proton.me>
2023-11-01 13:37:32 +0300

    adding mapping to add json attributes for Go structures

--- mappings.lua
+++ mappings.lua
@@ -27,12 +27,15 @@ return {
       end,
       desc = "Pick to close",
     },
+    ["<leader>gaj"] = { "<cmd>GoAddTag json<cr>", desc = "Add json to Go structure" },
+    ["<leader>grj"] = { "<cmd>GoRmTag json<cr>", desc = "Remove json from Go structure" },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
+
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
