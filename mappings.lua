43f6fd2d995c915585fec346564098e8e1b426c0
PainIsWhenSomeBodyDropT3 <thats_change_everything@proton.me>
2023-12-01 12:05:41 +0300

    fix mappings && remove autofill

--- mappings.lua
+++ mappings.lua
@@ -7,22 +7,33 @@ return {
   -- first key is the mode
   v = {
     ["<leader>p"] = '"_dP',
-    ["J"] = ":m '>+1<CR>gv=gv",
-    ["K"] = ":m '<-2<CR>gv=gv",
     ["<leader>y"] = '"+y',
+
+    ["J"] = ":m '>+1<CR><CR>gv=gv",
+    ["K"] = ":m '<-2<CR><CR>gv=gv",
   },
 
   n = {
-    -- second key is the lefthand side of the map
+    ["<leader>gaj"] = { "<cmd>GoAddTag json<cr>", desc = "Add json to Go structure" },
+    ["<leader>grj"] = { "<cmd>GoRmTag json<cr>", desc = "Remove json from Go structure" },
+
+    ["<leader>r"] = { "<cmd>!go run main.go<cr>", desc = "Run main.go" },
+
     ["<leader>s"] = ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
+
     ["<leader>x"] = "<cmd>!chmod +x %<CR>",
+
     ["J"] = "mzJ`z",
+
     ["<C-d>"] = "<C-d>zz",
     ["<C-u>"] = "<C-u>zz",
+
     ["n"] = "nzzzv",
     ["N"] = "Nzzzv",
+
     ["<leader>y"] = '"+y',
     ["<leader>Y"] = '"+Y',
+
     L = {
       function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
       desc = "Next buffer",
@@ -41,8 +52,6 @@ return {
       end,
       desc = "Pick to close",
     },
-    ["<leader>gaj"] = { "<cmd>GoAddTag json<cr>", desc = "Add json to Go structure" },
-    ["<leader>grj"] = { "<cmd>GoRmTag json<cr>", desc = "Remove json from Go structure" },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
